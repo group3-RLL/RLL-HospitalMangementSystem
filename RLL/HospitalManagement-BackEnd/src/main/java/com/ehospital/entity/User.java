bcacd0a33632884bba54ddd8680d3397e1a1f1f5
group6xyz <115799703+group6xyz@users.noreply.github.com>
2022-10-15 13:06:33 +0530

    Add files via upload

---
+++ RLL/HospitalManagement-BackEnd/src/main/java/com/ehospital/entity/User.java
@@ -0,0 +1,55 @@
+package com.ehospital.entity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "role")
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ RLL/HospitalManagement-FrontEnd/src/app/common/doctor.ts
@@ -0,0 +1,15 @@
+export class Doctor {
+    id: string;
+    name: string;
+    age: string;
+    gender: string;
+    blood: string;
+    dept: string
+    phone: string;
+    email: string;
+    status: string;
+    address: string;
+    room: string;
+
+
+}
